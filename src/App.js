bc0322fbbeb59cec8e24febcf64b21c3f204ed81
Binitha Babu <binithababu184@gmail.com>
2022-11-27 23:31:14 +0530

    bookclub

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
-import logo from './logo.svg';
+import Example from "./pages/Example/Example";
+import { Home } from "./pages/Home/Home";
+import { Cart } from "./pages/Cart/Cart";
+
+import { Routes, Route} from "react-router-dom";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+    <Route path="/example" element={<Example/>} />
+      <Route path="/" element={<Home />} />
+      <Route path="/cart" element={<Cart />} />
+      
+    </Routes>
+
   );
 }
 
---
+++ src/pages/Example/Example.jsx
@@ -0,0 +1,16 @@
+import "../Example/Example.css"
+import { Navbar } from "../../components";
+
+export default function Example()
+{
+    return (
+        
+<div>
+<Navbar/>
+<div className="container--example">
+<h1 className="text">The Book Club</h1>
+</div>
+</div>
+
+    );
+}
